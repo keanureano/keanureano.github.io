288ed0157d5450cbc62b7b25d3d709f228bf8fc1
Keanu Reaño <reanokeanu@gmail.com>
2024-07-02 19:37:57 +0800

    remove deprecated nextjs config setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
-
-let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true;
-
 const nextConfig = {
   output: process.env.NODE_ENV !== "production" ? undefined : "export",
-  images: {
-    unoptimized: envImageUnoptimize,
-  },
 };
 
 export default nextConfig;

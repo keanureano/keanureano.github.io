50d4d57fba8319d4d69d94731056551988854366
Keanu Reaño <reanokeanu@gmail.com>
2024-07-03 17:20:27 +0800

    update resumeSchema import to handle optional basics summary in page component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
+import { ResumeSchema } from "@/types/resumeSchema";
 import { promises as fs } from "fs";
 
 export default async function Home() {
   const file = await fs.readFile(process.cwd() + "/public/resume.json", "utf8");
-  const data = JSON.parse(file);
-  console.log(data);
+  const data: ResumeSchema = JSON.parse(file);
 
   return (
     <main>
       <h1>Hi there!</h1>
-      <p>{data.basics.summary}</p>
+      <p>{data.basics?.summary}</p>
     </main>
   );
 }
